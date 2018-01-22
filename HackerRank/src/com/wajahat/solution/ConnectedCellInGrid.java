ad312b6a01bd8a27835443d09b88bffdf8429911
WajahatSiddiqui <wajahatmeister@gmail.com>
2018-01-22 23:22:53 +0530

    add more solutions

--- HackerRank/src/com/wajahat/solution/ConnectedCellInGrid.java
+++ HackerRank/src/com/wajahat/solution/ConnectedCellInGrid.java
@@ -4,47 +4,57 @@ import java.util.Scanner;
 
 public class ConnectedCellInGrid {
 	
-	private static int findMaxRegion(int [][]mat, int row, int col) {
-		if (row < 0 || col < 0 || row >= mat.length || col >= mat[row].length || mat[row][col] == 0) {
-			return 0;
-		}
-		mat[row][col] = 0;
-		int size = 1;
-		for (int r = row - 1; r <= row + 1; r++) {
-			for (int c = col - 1; c <= col + 1; c++) {
-				if (r != row || c != col) {
-					size += findMaxRegion(mat, r, c);
-				}
-			}
-		}
-		return size;
+	private static boolean isConnected(int [][]mat, int n, int m, int x, int y) {
+		return x >= 0 && x < n && y >= 0 && y < m && mat[x][y] == 1;
 	}
+	
+	private static int dfs(int [][]mat, int x, int y, int n, int m, int []dx, int[] dy) {
+		mat[x][y] = 0;
+		for (int i = 0; i < dx.length; i++) {
+			int _x = x+dx[i];
+			int _y = y+dy[i];
+			if (isConnected(mat, n, m, _x, _y)) {
+				return dfs(mat, _x, _y, n, m, dx, dy) + 1;
+			}
+		}
+		return 1;
+	}
+	
+	private static int findConnectedCell(int [][]mat, int n, int m) {
+		int cell = 0;
 
-	private static int findConnectedCell(int [][]mat) {
-		int maxRegion = 0;
-		for (int row = 0; row < mat.length; row++) {
-			for (int col = 0; col < mat[row].length; col++) {
-				if (mat[row][col] == 1) {
-					int size = findMaxRegion(mat, row, col);
-					maxRegion = Math.max(size, maxRegion);
+		int []dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+		int []dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+			
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (mat[i][j] == 1) {
+					cell++;
+					cell = dfs(mat, i, j, n, m, dx, dy);
+					if (cell > max) {
+						max = cell;
+					}
+					cell = 0;
 				}
 			}
 		}
-		return maxRegion;
+		return max;
 	}
 
 	public static void main(String[] args) {
-		try (Scanner scanner = new Scanner(System.in)) {
-			int n = scanner.nextInt();
-			int m = scanner.nextInt();
-			int [][] mat = new int[n][m];
-			for (int i = 0; i < n; i++) {
-				for (int j = 0; j < m; j++) {
-					mat[i][j] = scanner.nextInt();
-				}
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int m = scanner.nextInt();
+		int [][] mat = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				mat[i][j] = scanner.nextInt();
 			}
-			System.out.println(findConnectedCell(mat));
 		}
+		System.out.println(findConnectedCell(mat, n, m));
+		scanner.close();
+
 	}
 
 }
---
+++ HackerRank/src/com/wajahat/solution/recursion/PowerSum.java
@@ -0,0 +1,10 @@
+package com.wajahat.solution.recursion;
+
+public class PowerSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
