6e33edb64edeb97de5864b4a20513fc19133efe5
WajahatSiddiqui <wajahatmeister@gmail.com>
2019-04-10 15:54:34 +0530

    add test cases for kadanes

--- Algorithms/src/com/wajahat/MaxSubArrayKadanesAlgorithm.java
+++ Algorithms/src/com/wajahat/MaxSubArrayKadanesAlgorithm.java
@@ -10,6 +10,22 @@ import static org.junit.Assert.assertEquals;
 
 public class MaxSubArrayKadanesAlgorithm {
 
+    public int kadane(final List<Integer> A) {
+        int N = A.size();
+        int maxSoFar = 0;
+        int maxEndingHere = 0;
+        for (int i = 0; i < N; i++) {
+            maxEndingHere += A.get(i);
+            if (maxEndingHere < 0) {
+                maxEndingHere = 0;
+            } else if (maxEndingHere > maxSoFar) {
+                maxSoFar = maxEndingHere;
+            }
+        }
+        return maxSoFar;
+    }
+
+    // Handling all negatives
     public int maxSubArray(final List<Integer> A) {
         int maxSoFar = A.get(0);
         int currMax = A.get(0);
@@ -21,6 +37,14 @@ public class MaxSubArrayKadanesAlgorithm {
             maxSoFar = Math.max(currMax, maxSoFar);
         }
         return maxSoFar;
+    }
+
+    @Test
+    public void testKadane() {
+        assertEquals(6, kadane(Stream.of(-2,1,-3,4,-1,2,1,-5,4).collect(Collectors.toList())));
+        assertEquals(0, kadane(Stream.of(-2,-1,-3,-4,-1,-2,-1,-5,-4).collect(Collectors.toList())));
+        assertEquals(0, kadane(Stream.of(-2).collect(Collectors.toList())));
+        assertEquals(1000, kadane(Stream.of(1000).collect(Collectors.toList())));
     }
 
     @Test
