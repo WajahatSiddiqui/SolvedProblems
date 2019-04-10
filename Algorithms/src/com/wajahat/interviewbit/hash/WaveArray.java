5bdfacdd495542c050ab7e9e951296eb255d4e42
WajahatSiddiqui <wajahatmeister@gmail.com>
2019-04-10 21:53:06 +0530

    Problem solutions from interviewbit

---
+++ Algorithms/src/com/wajahat/interviewbit/hash/WaveArray.java
@@ -0,0 +1,29 @@
+
+package com.wajahat.interviewbit.hash;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class WaveArray {
+    public ArrayList<Integer> wave(ArrayList<Integer> A) {
+        Collections.sort(A);
+        int N = A.size();
+
+        for (int i = 0; i < N-1; i+=2) {
+            int tmp = A.get(i);
+            A.set(i, A.get(i+1));
+            A.set(i+1, tmp);
+        }
+        return A;
+    }
+
+    @Test
+    public void testWave() {
+        System.out.print(wave(new ArrayList<>(Stream.of(3, 2, 1, 4).collect(Collectors.toList()))));
+        System.out.print(wave(new ArrayList<>(Stream.of( 5, 1, 3, 2, 4 ).collect(Collectors.toList()))));
+    }
+}
